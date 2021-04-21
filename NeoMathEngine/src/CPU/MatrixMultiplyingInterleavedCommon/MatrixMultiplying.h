f57f0b5191ee7317b8cc0d3e7f5211d4b0f5a13d
Yury Katkov <ykatkov@yandex.ru>
2021-04-21 13:24:34 +0300

    Added my kernel 6x16
    Signed-off-by: Yury Katkov <ykatkov@yandex.ru>

--- NeoMathEngine/src/CPU/MatrixMultiplyingInterleavedCommon/MatrixMultiplying.h
+++ NeoMathEngine/src/CPU/MatrixMultiplyingInterleavedCommon/MatrixMultiplying.h
@@ -67,19 +67,174 @@ inline void _mm256_storeu2_m128( float *hiAddr, float *loAddr, __m256 data )
 
 struct CMicroKernel_6x16 : public CMicroKernelBase<6, 16> {
 	static void Calculate( const float* aPtr, const float* bPtr, float* cPtr, size_t cRowSize, size_t k ) {
-		float alpha = 1.0;
-		float beta = 1.0;
-		neo_sgemm_haswell_asm_6x16
-			 (
-			   k,
-			   &alpha,
-			   const_cast<float*>(aPtr),
-			   const_cast<float*>(bPtr),
-			   &beta,
-			   cPtr, cRowSize, 1,
-			   0,
-			   0
-			 );
+		_mm_prefetch( cPtr + 0 * cRowSize, _MM_HINT_T0 );
+		_mm_prefetch( cPtr + 1 * cRowSize, _MM_HINT_T0 );
+		_mm_prefetch( cPtr + 2 * cRowSize, _MM_HINT_T0 );
+		_mm_prefetch( cPtr + 3 * cRowSize, _MM_HINT_T0 );
+		_mm_prefetch( cPtr + 4 * cRowSize, _MM_HINT_T0 );
+		_mm_prefetch( cPtr + 5 * cRowSize, _MM_HINT_T0 );
+		__m256 c00 = _mm256_setzero_ps();
+		__m256 c01 = _mm256_setzero_ps();
+		__m256 c10 = _mm256_setzero_ps();
+		__m256 c11 = _mm256_setzero_ps();
+		__m256 c20 = _mm256_setzero_ps();
+		__m256 c21 = _mm256_setzero_ps();
+		__m256 c30 = _mm256_setzero_ps();
+		__m256 c31 = _mm256_setzero_ps();
+		__m256 c40 = _mm256_setzero_ps();
+		__m256 c41 = _mm256_setzero_ps();
+		__m256 c50 = _mm256_setzero_ps();
+		__m256 c51 = _mm256_setzero_ps();
+
+		__m256 b0, b1, a0, a1;
+
+		for( ; k >= 4; k -= 4 ) {
+			// Iteration 0
+			_mm_prefetch( aPtr + 80, _MM_HINT_T0 );
+			b0 = _mm256_loadu_ps( bPtr + 0 );
+			b1 = _mm256_loadu_ps( bPtr + 8 );
+			a0 = _mm256_broadcast_ss( aPtr + 0 );
+			a1 = _mm256_broadcast_ss( aPtr + 1 );
+			c00 = _mm256_fmadd_ps( a0, b0, c00 );
+			c01 = _mm256_fmadd_ps( a0, b1, c01 );
+			c10 = _mm256_fmadd_ps( a1, b0, c10 );
+			c11 = _mm256_fmadd_ps( a1, b1, c11 );
+
+			a0 = _mm256_broadcast_ss( aPtr + 2 );
+			a1 = _mm256_broadcast_ss( aPtr + 3 );
+			c20 = _mm256_fmadd_ps( a0, b0, c20 );
+			c21 = _mm256_fmadd_ps( a0, b1, c21 );
+			c30 = _mm256_fmadd_ps( a1, b0, c30 );
+			c31 = _mm256_fmadd_ps( a1, b1, c31 );
+
+			a0 = _mm256_broadcast_ss( aPtr + 4 );
+			a1 = _mm256_broadcast_ss( aPtr + 5 );
+			c40 = _mm256_fmadd_ps( a0, b0, c40 );
+			c41 = _mm256_fmadd_ps( a0, b1, c41 );
+			c50 = _mm256_fmadd_ps( a1, b0, c50 );
+			c51 = _mm256_fmadd_ps( a1, b1, c51 );
+
+			// Iteration 1
+			b0 = _mm256_loadu_ps( bPtr + 16 );
+			b1 = _mm256_loadu_ps( bPtr + 24 );
+			a0 = _mm256_broadcast_ss( aPtr + 6 );
+			a1 = _mm256_broadcast_ss( aPtr + 7 );
+			c00 = _mm256_fmadd_ps( a0, b0, c00 );
+			c01 = _mm256_fmadd_ps( a0, b1, c01 );
+			c10 = _mm256_fmadd_ps( a1, b0, c10 );
+			c11 = _mm256_fmadd_ps( a1, b1, c11 );
+
+			a0 = _mm256_broadcast_ss( aPtr + 8 );
+			a1 = _mm256_broadcast_ss( aPtr + 9 );
+			c20 = _mm256_fmadd_ps( a0, b0, c20 );
+			c21 = _mm256_fmadd_ps( a0, b1, c21 );
+			c30 = _mm256_fmadd_ps( a1, b0, c30 );
+			c31 = _mm256_fmadd_ps( a1, b1, c31 );
+
+			a0 = _mm256_broadcast_ss( aPtr + 10 );
+			a1 = _mm256_broadcast_ss( aPtr + 11 );
+			c40 = _mm256_fmadd_ps( a0, b0, c40 );
+			c41 = _mm256_fmadd_ps( a0, b1, c41 );
+			c50 = _mm256_fmadd_ps( a1, b0, c50 );
+			c51 = _mm256_fmadd_ps( a1, b1, c51 );
+
+			// Iteration 2
+			_mm_prefetch( aPtr + 76, _MM_HINT_T0 );
+			b0 = _mm256_loadu_ps( bPtr + 32 );
+			b1 = _mm256_loadu_ps( bPtr + 40 );
+			a0 = _mm256_broadcast_ss( aPtr +12 );
+			a1 = _mm256_broadcast_ss( aPtr + 13 );
+			c00 = _mm256_fmadd_ps( a0, b0, c00 );
+			c01 = _mm256_fmadd_ps( a0, b1, c01 );
+			c10 = _mm256_fmadd_ps( a1, b0, c10 );
+			c11 = _mm256_fmadd_ps( a1, b1, c11 );
+
+			a0 = _mm256_broadcast_ss( aPtr + 14 );
+			a1 = _mm256_broadcast_ss( aPtr + 15 );
+			c20 = _mm256_fmadd_ps( a0, b0, c20 );
+			c21 = _mm256_fmadd_ps( a0, b1, c21 );
+			c30 = _mm256_fmadd_ps( a1, b0, c30 );
+			c31 = _mm256_fmadd_ps( a1, b1, c31 );
+
+			a0 = _mm256_broadcast_ss( aPtr + 16 );
+			a1 = _mm256_broadcast_ss( aPtr + 17 );
+			c40 = _mm256_fmadd_ps( a0, b0, c40 );
+			c41 = _mm256_fmadd_ps( a0, b1, c41 );
+			c50 = _mm256_fmadd_ps( a1, b0, c50 );
+			c51 = _mm256_fmadd_ps( a1, b1, c51 );
+
+			// Iteration 3
+			b0 = _mm256_loadu_ps( bPtr + 48 );
+			b1 = _mm256_loadu_ps( bPtr + 56 );
+			a0 = _mm256_broadcast_ss( aPtr + 18 );
+			a1 = _mm256_broadcast_ss( aPtr + 19 );
+			c00 = _mm256_fmadd_ps( a0, b0, c00 );
+			c01 = _mm256_fmadd_ps( a0, b1, c01 );
+			c10 = _mm256_fmadd_ps( a1, b0, c10 );
+			c11 = _mm256_fmadd_ps( a1, b1, c11 );
+
+			a0 = _mm256_broadcast_ss( aPtr + 20 );
+			a1 = _mm256_broadcast_ss( aPtr + 21 );
+			c20 = _mm256_fmadd_ps( a0, b0, c20 );
+			c21 = _mm256_fmadd_ps( a0, b1, c21 );
+			c30 = _mm256_fmadd_ps( a1, b0, c30 );
+			c31 = _mm256_fmadd_ps( a1, b1, c31 );
+
+			a0 = _mm256_broadcast_ss( aPtr + 22 );
+			a1 = _mm256_broadcast_ss( aPtr + 23 );
+			c40 = _mm256_fmadd_ps( a0, b0, c40 );
+			c41 = _mm256_fmadd_ps( a0, b1, c41 );
+			c50 = _mm256_fmadd_ps( a1, b0, c50 );
+			c51 = _mm256_fmadd_ps( a1, b1, c51 );
+
+
+			bPtr += 64; aPtr += 24;
+		}
+
+		for( ; k > 0; k-- ) {
+			b0 = _mm256_loadu_ps( bPtr + 0 );
+			b1 = _mm256_loadu_ps( bPtr + 8 );
+			a0 = _mm256_broadcast_ss( aPtr + 0 );
+			a1 = _mm256_broadcast_ss( aPtr + 1 );
+			c00 = _mm256_fmadd_ps( a0, b0, c00 );
+			c01 = _mm256_fmadd_ps( a0, b1, c01 );
+			c10 = _mm256_fmadd_ps( a1, b0, c10 );
+			c11 = _mm256_fmadd_ps( a1, b1, c11 );
+
+			a0 = _mm256_broadcast_ss( aPtr + 2 );
+			a1 = _mm256_broadcast_ss( aPtr + 3 );
+			c20 = _mm256_fmadd_ps( a0, b0, c20 );
+			c21 = _mm256_fmadd_ps( a0, b1, c21 );
+			c30 = _mm256_fmadd_ps( a1, b0, c30 );
+			c31 = _mm256_fmadd_ps( a1, b1, c31 );
+
+			a0 = _mm256_broadcast_ss( aPtr + 4 );
+			a1 = _mm256_broadcast_ss( aPtr + 5 );
+			c40 = _mm256_fmadd_ps( a0, b0, c40 );
+			c41 = _mm256_fmadd_ps( a0, b1, c41 );
+			c50 = _mm256_fmadd_ps( a1, b0, c50 );
+			c51 = _mm256_fmadd_ps( a1, b1, c51 );
+
+			bPtr += 16; aPtr += 6;
+		}
+
+		_mm256_storeu_ps( cPtr, _mm256_add_ps( c00, _mm256_loadu_ps( cPtr ) ) );
+		_mm256_storeu_ps( cPtr + 8, _mm256_add_ps( c01, _mm256_loadu_ps( cPtr + 8 ) ) );
+		cPtr += cRowSize;
+		_mm256_storeu_ps( cPtr, _mm256_add_ps( c10, _mm256_loadu_ps( cPtr ) ) );
+		_mm256_storeu_ps( cPtr + 8, _mm256_add_ps( c11, _mm256_loadu_ps( cPtr + 8 ) ) );
+		cPtr += cRowSize;
+		_mm256_storeu_ps( cPtr, _mm256_add_ps( c20, _mm256_loadu_ps( cPtr ) ) );
+		_mm256_storeu_ps( cPtr + 8, _mm256_add_ps( c21, _mm256_loadu_ps( cPtr + 8 ) ) );
+		cPtr += cRowSize;
+		_mm256_storeu_ps( cPtr, _mm256_add_ps( c30, _mm256_loadu_ps( cPtr ) ) );
+		_mm256_storeu_ps( cPtr + 8, _mm256_add_ps( c31, _mm256_loadu_ps( cPtr + 8 ) ) );
+		cPtr += cRowSize;
+		_mm256_storeu_ps( cPtr, _mm256_add_ps( c40, _mm256_loadu_ps( cPtr ) ) );
+		_mm256_storeu_ps( cPtr + 8, _mm256_add_ps( c41, _mm256_loadu_ps( cPtr + 8 ) ) );
+		cPtr += cRowSize;
+		_mm256_storeu_ps( cPtr, _mm256_add_ps( c50, _mm256_loadu_ps( cPtr ) ) );
+		_mm256_storeu_ps( cPtr + 8, _mm256_add_ps( c51, _mm256_loadu_ps( cPtr + 8 ) ) );
 	}
 };
 
@@ -568,16 +723,14 @@ inline void MultiplyMatrix(Engine *engine, const CCPUInfo &cpuInfo,
 	float* cPtr, size_t cRowSize,
 	size_t m, size_t n, size_t k)
 {
-	if( getenv("MYGEMM") != 0) {
-		CMatrixMultiplier<CMicroKernel_6x16, CInterleaverDefault, ATransposed, BTransposed, MemoryHandler, Engine>::MyMultiply
-			(engine, cpuInfo, aPtr, aRowSize, bPtr, bRowSize, cPtr, cRowSize, m, n, k);
-	} else {
+//	if( getenv("MYGEMM") != 0) {
+//		CMatrixMultiplier<CMicroKernel_6x16, CInterleaverDefault, ATransposed, BTransposed, MemoryHandler, Engine>::MyMultiply
+//			(engine, cpuInfo, aPtr, aRowSize, bPtr, bRowSize, cPtr, cRowSize, m, n, k);
+//	} else {
 		if( HasMyKernel ) {
-			CMatrixMultiplier<CKernelCombi, CInterleaverDefault, ATransposed, BTransposed, MemoryHandler, Engine>::Multiply
-				(engine, cpuInfo, aPtr, aRowSize, bPtr, bRowSize, cPtr, cRowSize, m, n, k);
 		} else {
 			CMatrixMultiplier<CMicroKernel_6x16, CInterleaverDefault, ATransposed, BTransposed, MemoryHandler, Engine>::Multiply
 				(engine, cpuInfo, aPtr, aRowSize, bPtr, bRowSize, cPtr, cRowSize, m, n, k);
 		}
-	}
+//	}
 }
